ebb8085ae9b341ceb377cdd89fc422a25687332c
Jeff Simons Decena <jeff.decena@yahoo.com>
2017-08-19 17:11:32 +0800

    Create the profile and orders page

--- resources/views/front/accounts.blade.php
+++ resources/views/front/accounts.blade.php
@@ -6,6 +6,93 @@
         <div class="row">
             <div class="col-md-12">
                 <h2> <i class="fa fa-home"></i> My Account</h2>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+                        <li role="presentation"><a href="#orders" aria-controls="orders" role="tab" data-toggle="tab">Orders</a></li>
+                        <li role="presentation"><a href="#address" aria-controls="address" role="tab" data-toggle="tab">Addresses</a></li>
+                    </ul>
+
+                    <!-- Tab panes -->
+                    <div class="tab-content customer-order-list">
+                        <div role="tabpanel" class="tab-pane active" id="profile">
+                            {{$customer->name}} <br /><small>{{$customer->email}}</small>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="orders">
+                            <table class="table">
+                                <tbody>
+                                <tr>
+                                    <td class="col-md-3">Date</td>
+                                    <td class="col-md-2">Courier</td>
+                                    <td class="col-md-2">Total</td>
+                                    <td class="col-md-2">Status</td>
+                                </tr>
+                                </tbody>
+                                <tbody>
+                                @foreach ($orders as $order)
+                                    <tr>
+                                        <td>
+                                            <a data-toggle="modal" data-target="#order_modal_{{$order->id}}" title="Show order" href="javascript: void(0)">{{ date('M d, Y h:i a', strtotime($order->created_at)) }}</a>
+                                            <!-- Button trigger modal -->
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="order_modal_{{$order->id}}" tabindex="-1" role="dialog" aria-labelledby="MyOrders">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                            <h4 class="modal-title" id="myModalLabel">Reference #{{$order->reference}}</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <table class="table">
+                                                                <thead>
+                                                                    <th>Address</th>
+                                                                    <th>Payment</th>
+                                                                    <th>Total</th>
+                                                                    <th>Status</th>
+                                                                </thead>
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td>
+                                                                            <address>
+                                                                                <strong>{{$order->address->alias}}</strong><br />
+                                                                                {{$order->address->address_1}} {{$order->address->address_2}}<br>
+                                                                                San Francisco, CA 94103<br>
+                                                                                <abbr title="Phone">P:</abbr> (123) 456-7890
+                                                                            </address>
+                                                                        </td>
+                                                                        <td>{{$order->payment->name}}</td>
+                                                                        <td>{{$order->total}}</td>
+                                                                        <td>{{$order->status->name}}</td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>{{ $order->courier->name }}</td>
+                                        <td><span class="label @if($order->total != $order->total_paid) label-danger @else label-success @endif">Php {{ $order->total }}</span></td>
+                                        <td><p class="text-center" style="color: #ffffff; background-color: {{ $order->status->color }}">{{ $order->status->name }}</p></td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="address">...</div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </section>
---
+++ resources/views/front/shared/sidebar.blade.php
@@ -0,0 +1,5 @@
+<div class="col-md-4">
+    <ul class="list-unstyled">
+        <li><a href="#">My orders</a></li>
+    </ul>
+</div>
