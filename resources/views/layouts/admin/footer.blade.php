d8497c930ec7bbf13200fb4d3833257351d0b64f
Jeff Simons Decena <jeff.decena@yahoo.com>
2017-08-30 11:29:59 +0800

    Fix footer with generic app name

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -2,6 +2,6 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 0.1.0
     </div>
-    <strong>Copyright &copy; {{ date('Y') }} - {{ date('Y') + 1 }} <a href="http://firstwelve.com">FirsTwelve</a>.</strong> All rights
+    <strong>Copyright &copy; {{ date('Y') }} - {{ date('Y') + 1 }} <a href="{{config('app.url')}}">{{config('app.name')}}</a>.</strong> All rights
     reserved.
 </footer>
