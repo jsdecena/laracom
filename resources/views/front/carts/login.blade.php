a7a8e763e2c09fc735a878435c4e11d2e2becfb5
Jeff Simons Decena <jeff.decena@yahoo.com>
2017-09-12 15:23:20 +0800

    Fix styling on cart login and update composer
    - Fix some styling on the cart login page to make it more intuitive
    - Add badge in readme and update keywords in composer

--- resources/views/front/carts/login.blade.php
+++ resources/views/front/carts/login.blade.php
@@ -6,12 +6,12 @@
     <section class="container content">
         <div class="row">
             <div class="col-md-12">@include('layouts.errors-and-messages')</div>
-            <div class="col-md-4 col-md-offset-4">
+            <div class="col-md-5">
                 <h2>Login to your account</h2>
                 <form action="{{ route('cart.login') }}" method="post" class="form-horizontal">
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <label for="email">Email</label>
+                        <label for="email" class="control-label">Email</label>
                         <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="Email" autofocus>
                     </div>
                     <div class="form-group">
@@ -19,14 +19,41 @@
                         <input type="password" name="password" id="password" value="" class="form-control" placeholder="xxxxx">
                     </div>
                     <div class="row">
-                        <button class="btn btn-default btn-block" type="submit">Login now</button>
+                        <button class="btn btn-primary btn-block" type="submit">Login now</button>
                     </div>
                 </form>
-                <div class="row">
-                    <hr>
+                <div class="row"><hr>
                     <a href="#">I forgot my password</a><br>
-                    <a href="{{ url('/') }}" class="text-center">No account? Register here.</a>
                 </div>
+            </div>
+            <div class="col-md-6 col-md-offset-1">
+                <h2>Register an account</h2>
+                <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="name" class="control-label">Name</label>
+                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email" class="control-label">E-Mail Address</label>
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password" class="control-label">Password</label>
+                        <input id="password" type="password" class="form-control" name="password" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password-confirm" class="control-label">Confirm Password</label>
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    </div>
+                </form>
             </div>
         </div>
     </section>
