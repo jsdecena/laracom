6a4793bbf4ce9bf7a79d01963a4e2cf4d986bb18
Jeff Simons Decena <jeff.decena@yahoo.com>
2018-02-21 22:22:20 +0800

    Modify index page to look better

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -14,6 +14,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="{{ asset('front/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('front/style.css') }}" rel="stylesheet">
 
--- resources/views/layouts/front/home-slider.blade.php
+++ resources/views/layouts/front/home-slider.blade.php
@@ -4,8 +4,9 @@
             <div class="col-md-8">
                 <div class="hero-content">
                     <h1 class="hero-title">{{config('app.name')}} Holiday <br> Collection</h1>
-                    <p class="hero-text">Download this amazing e-commerce web app for <strong class="text-danger">FREE!</strong></p>
-                    <a class="btn btn-succes btn-hero" href="https://github.com/jsdecena/laracom" target="_blank" role="button">DOWNLOAD <i class="fa fa-cloud-download"></i></a>
+                    <p class="hero-text">Download this amazing e-commerce web app for <strong class="text-success">FREE!</strong></p>
+                    <a class="btn btn-success" href="https://github.com/jsdecena/laracom" target="_blank" role="button">DOWNLOAD <i class="fa fa-cloud-download"></i></a>
+                    <a class="btn btn-danger" href="https://m.do.co/c/bce94237de96" target="_blank" role="button">Build on DigitalOcean <i class="fa fa-paper-plane"></i></a>
                 </div>
             </div>
             <div class="col-md-4"></div>
