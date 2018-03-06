16ca41b1f28deb0663db3d145561595417ce383c
Jeff Simons Decena <jeff.decena@yahoo.com>
2018-03-06 19:31:14 +0800

    Add facebook og tags

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.front.app')
 
+@section('og')
+    <meta property="og:type" content="home"/>
+    <meta property="og:title" content="{{ config('app.name') }}"/>
+    <meta property="og:description" content="{{ config('app.name') }}"/>
+@endsection
+
 @section('content')
     @include('layouts.front.home-slider')
 
