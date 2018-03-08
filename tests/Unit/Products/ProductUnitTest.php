3249539c9620f11fa9f0b36c58cf2fb418ca0fff
Jeff Simons Decena <jeff.decena@yahoo.com>
2018-03-08 19:01:52 +0800

    Create a testcase for image upload

--- tests/Unit/Products/ProductUnitTest.php
+++ tests/Unit/Products/ProductUnitTest.php
@@ -4,16 +4,53 @@ namespace Tests\Unit\Products;
 
 use App\Shop\Categories\Category;
 use App\Shop\ProductImages\ProductImage;
-use App\Shop\ProductImages\ProductImageRepository;
 use App\Shop\Products\Exceptions\ProductInvalidArgumentException;
 use App\Shop\Products\Exceptions\ProductNotFoundException;
 use App\Shop\Products\Product;
 use App\Shop\Products\Repositories\ProductRepository;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class ProductUnitTest extends TestCase
 {
+    /** @test */
+    public function it_can_save_the_thumbnails_properly_in_the_file_storage()
+    {
+        $thumbnails = [
+            UploadedFile::fake()->image('cover.jpg', 600, 600),
+            UploadedFile::fake()->image('cover.jpg', 600, 600),
+            UploadedFile::fake()->image('cover.jpg', 600, 600)
+        ];
+
+        $collection = collect($thumbnails);
+
+        $product = factory(Product::class)->create();
+        $productRepo = new ProductRepository($product);
+        $productRepo->saveProductImages($collection, $product);
+
+        $images = $productRepo->findProductImages();
+
+        $images->each(function (ProductImage $image) {
+            $exists = Storage::disk('public')->exists($image->src);
+            $this->assertTrue($exists);
+        });
+    }
+
+    /** @test */
+    public function it_can_save_the_cover_image_properly_in_file_storage()
+    {
+        $cover = UploadedFile::fake()->image('cover.jpg', 600, 600);
+
+        $product = factory(Product::class)->create();
+        $productRepo = new ProductRepository($product);
+        $filename = $productRepo->saveCoverImage($cover);
+
+        $exists = Storage::disk('public')->exists($filename);
+
+        $this->assertTrue($exists);
+    }
+
     /** @test */
     public function it_can_detach_all_the_categories()
     {
@@ -30,7 +67,7 @@ class ProductUnitTest extends TestCase
 
         $this->assertCount(4, $productRepo->getCategories());
 
-        $productRepo->detachCategories($product);
+        $productRepo->detachCategories();
 
         $this->assertCount(0, $productRepo->getCategories());
     }
