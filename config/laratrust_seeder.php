22ddfc0fd90d7b9f9756e11a3ed93bebe81e5555
Jeff Simons Decena <jeff.decena@yahoo.com>
2018-03-10 10:22:29 +0800

    Install the laratrust package

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'role_structure' => [
+        'superadministrator' => [
+            'users' => 'c,r,u,d',
+            'acl' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'administrator' => [
+            'users' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'user' => [
+            'profile' => 'r,u'
+        ],
+    ],
+    'permission_structure' => [
+        'cru_user' => [
+            'profile' => 'c,r,u'
+        ],
+    ],
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
