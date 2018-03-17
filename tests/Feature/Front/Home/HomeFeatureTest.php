437927b7f71e2e39b89dcda7084b21001e26edfa
Jeff Simons Decena <jeff.decena@yahoo.com>
2018-03-17 08:11:41 +0800

    Create seeder for homepage products

--- tests/Feature/Front/Home/HomeFeatureTest.php
+++ tests/Feature/Front/Home/HomeFeatureTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Front\Home;
 
 use App\Shop\Categories\Category;
+use App\Shop\Products\Product;
 use Tests\TestCase;
 
 class HomeFeatureTest extends TestCase
@@ -10,10 +11,32 @@ class HomeFeatureTest extends TestCase
     /** @test */
     public function it_should_show_the_homepage()
     {
-        factory(Category::class, 3)->create();
+        factory(Category::class)->create([
+            'name' => 'New Arrivals',
+            'slug' => 'new-arrivals',
+            'status' => 1
+        ])->each(function (Category $category) {
+            factory(Product::class, 3)->make()->each(function(Product $product) use ($category) {
+                $category->products()->save($product);
+            });
+        });
+
+        factory(Category::class)->create([
+            'name' => 'Featured',
+            'slug' => 'featured',
+            'status' => 1
+        ])->each(function (Category $category) {
+            factory(Product::class, 3)->make()->each(function(Product $product) use ($category) {
+                $category->products()->save($product);
+            });
+        });
 
         $this
             ->get(route('home'))
+            ->assertSee('Login')
+            ->assertSee('Register')
+            ->assertSee('New Arrivals')
+            ->assertSee('Featured Products')
             ->assertStatus(200);
     }
 }
