cb1fae6ab26c04071287b1eeb3300325198aafe5
Jeff Simons Decena <jeff.decena@yahoo.com>
2018-03-17 22:36:05 +0800

    Add updated shipping fee in stripe payment

--- resources/views/layouts/front/payment-options.blade.php
+++ resources/views/layouts/front/payment-options.blade.php
@@ -14,21 +14,14 @@
         </td>
         <td>
             @if(isset($payment['name']))
-                <form action="{{ route('checkout.execute') }}" method="post" class="pull-right">
+                <form action="{{ route('checkout.execute') }}" method="post" class="pull-right" id="stripeForm">
+                    <input type="hidden" name="payment" value="{{ config('stripe.name') }}">
+                    <input type="hidden" name="stripeToken" value="">
                     <input type="hidden" class="address_id" name="billing_address" value="">
                     <input type="hidden" class="delivery_address_id" name="delivery_address" value="">
                     <input type="hidden" class="courier_id" name="courier" value="">
                     {{ csrf_field() }}
-                    <script
-                            src="{{ url('https://checkout.stripe.com/checkout.js') }}" class="stripe-button"
-                            data-key="{{ config('stripe.key') }}"
-                            data-amount="{{ $total * 100 }}"
-                            data-name="{{ ucwords(config('stripe.name')) }}"
-                            data-description="{{ config('stripe.description') }}"
-                            data-image="{{ url('https://stripe.com/img/documentation/checkout/marketplace.png') }}"
-                            data-locale="auto"
-                            data-currency="{{ config('cart.currency') }}">
-                    </script>
+                    <button id="paywithstripe" class="btn btn-primary">Pay with Stripe <i class="fa fa-cc-stripe"></i></button>
                 </form>
             @endif
         </td>
@@ -48,12 +41,13 @@
             @endif
         </td>
         <td>
-            <form action="{{ route('checkout.store') }}" method="post">
+            <form action="{{ route('checkout.store') }}" method="post" class="pull-right" id="payPalForm">
                 {{ csrf_field() }}
                 <input type="hidden" name="payment" value="{{ config('paypal.name') }}">
                 <input type="hidden" class="address_id" name="billing_address" value="">
                 <input type="hidden" class="delivery_address_id" name="delivery_address" value="">
                 <input type="hidden" class="courier_id" name="courier" value="">
+                <input type="hidden" id="shippingFeeC" value="0">
                 <button type="submit" class="btn btn-success pull-right">Pay with PayPal <i class="fa fa-paypal"></i></button>
             </form>
         </td>
@@ -61,6 +55,9 @@
 @endif
 
 @section('js')
+
+    <script src="{{ url('https://checkout.stripe.com/checkout.js') }}"></script>
+
     <script type="text/javascript">
 
         function setTotal(total, shippingCost) {
@@ -69,7 +66,9 @@
         }
 
         function setShippingFee(cost) {
-            $('#shippingFee').html(cost);
+            el = '#shippingFee';
+            $(el).html(cost);
+            $('#shippingFeeC').val(cost);
         }
 
         function setCourierDetails(courierId) {
@@ -121,6 +120,38 @@
                 setCourierDetails(courierId);
                 setTotal(total, shippingCost);
             }
+
+            var handler = StripeCheckout.configure({
+                key: "{{ config('stripe.key') }}",
+                locale: 'auto',
+                token: function(token) {
+                    // You can access the token ID with `token.id`.
+                    // Get the token ID to your server-side code for use.
+                    console.log(token.id);
+                    $('input[name="stripeToken"]').val(token.id);
+                    $('#stripeForm').submit();
+                }
+            });
+
+            document.getElementById('paywithstripe').addEventListener('click', function(e) {
+                var total = parseFloat("{{ $total }}");
+                var shipping = parseFloat($('#shippingFeeC').val());
+                var amount = total + shipping;
+                // Open Checkout with further options:
+                handler.open({
+                    name: "{{ config('stripe.name') }}",
+                    description: "{{ config('stripe.description') }}",
+                    currency: "{{ config('cart.currency') }}",
+                    amount: amount * 100,
+                    email: "{{ $customer->email }}"
+                });
+                e.preventDefault();
+            });
+
+            // Close Checkout on page navigation:
+            window.addEventListener('popstate', function() {
+                handler.close();
+            });
         });
     </script>
 @endsection
