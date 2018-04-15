a9bb18e4fbb836b6b88e10bf4183d7ac54fbe1db
Enrico Romeo <enricoromeo@MacBook-Pro-di-Enrico.local>
2018-04-15 20:50:59 +0200

    fix create product images migration add onDelete('cascade') in foreign('product_id')

--- database/migrations/2018_01_18_163143_create_product_images_table.php
+++ database/migrations/2018_01_18_163143_create_product_images_table.php
@@ -16,7 +16,7 @@ class CreateProductImagesTable extends Migration
         Schema::create('product_images', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->unsigned()->index();
-            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->string('src');
         });
     }
