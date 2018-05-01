5f3d915627432c0cfa3f1a069a9852da3bf230e2
zoujiaqing <zoujiaqing@gmail.com>
2018-05-01 20:14:59 +0800

    fix artisan create tables by utf8mb4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Laravel\Cashier\Cashier;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Cashier::useCurrency(config('cart.currency'), config('cart.currency_symbol'));
     }
 
