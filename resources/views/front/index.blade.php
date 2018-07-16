7e0685501a9b417fa79a41d3d58fada3fb0334a9
Jeff Simons Decena <jeff.decena@yahoo.com>
2018-07-16 19:41:14 +0800

    Do not show the disabled products

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -15,7 +15,7 @@
                 <div class="section-title b50">
                     <h2>{{ $cat1->name }}</h2>
                 </div>
-                @include('front.products.product-list', ['products' => $cat1->products])
+                @include('front.products.product-list', ['products' => $cat1->products->where('status', 1)])
                 <div id="browse-all-btn"> <a class="btn btn-default browse-all-btn" href="{{ route('front.category.slug', $cat1->slug) }}" role="button">browse all items</a></div>
             </div>
         </section>
@@ -26,7 +26,7 @@
             <div class="section-title b100">
                 <h2>{{ $cat2->name }}</h2>
             </div>
-            @include('front.products.product-list', ['products' => $cat2->products])
+            @include('front.products.product-list', ['products' => $cat2->products->where('status', 1)])
             <div id="browse-all-btn"> <a class="btn btn-default browse-all-btn" href="{{ route('front.category.slug', $cat2->slug) }}" role="button">browse all items</a></div>
         </div>
     @endif
