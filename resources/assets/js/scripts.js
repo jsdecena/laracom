7f78f70f471fab558a03190d5d31b23a5f37dc7d
mufaddalnagpurwala <mufaddal.nagpurwala@cactusglobal.com>
2018-08-14 18:15:11 +0530

    BNS-11 - Variation Image - I

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -21,4 +21,13 @@ $(document).ready(function () {
         ],
         'sorting' : []
     });
+
+    $('.btn-radio').click(function(e) {
+        $('.btn-radio').not(this).removeClass('active')
+            .siblings('input').prop('checked',false)
+            .siblings('.img-radio').css('opacity','0.5');
+        $(this).addClass('active')
+            .siblings('input').prop('checked',true)
+            .siblings('.img-radio').css('opacity','1');
+    });
 });
--- resources/views/admin/products/attributes.blade.php
+++ resources/views/admin/products/attributes.blade.php
@@ -8,7 +8,9 @@
                     <th>Quantity</th>
                     <th>Price</th>
                     <th>Attributes</th>
-                    <th>Remove</th>
+                    <th>Image</th>
+                    <th>Add Image</th>
+                    <th>Remove Variation</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -23,6 +25,51 @@
                                     <li>{{ $item->attribute->name }} : {{ $item->value }}</li>
                                 @endforeach
                             </ul>
+                        </td>
+                        <td></td>
+                        <td>
+                            <div class="center">
+                                <a href="#" data-toggle="modal" data-target="#squarespaceModal-{{ $pa->id }}" class="btn btn-secondary center-block">Add Image
+                                </a>
+                            </div>
+                            <div class="modal fade" id="squarespaceModal-{{ $pa->id }}" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+                                            <h3 class="modal-title" id="lineModalLabel">Add Variation Image</h3>
+                                        </div>
+                                        <div class="modal-body">
+
+                                            <!-- content goes here -->
+                                            <form>
+                                                <div class="form-group">
+                                                    <label for="exampleInputEmail1">Product Images</label>
+                                                    <div class="row">
+                                                        @if(isset($product->cover))
+                                                        <div class="col-xs-4">
+                                                            <img src="{{ asset("storage/$product->cover") }}" class="img-responsive img-radio">
+                                                            <button type="button" class="btn btn-primary btn-radio">Select</button>
+                                                            <input type="checkbox" id="left-item" class="hidden" name="product_cover" value="{{ $product->id }}">
+                                                        </div>
+                                                        @endif
+                                                        @if($images && !empty($images))
+                                                            @foreach($images as $image)
+                                                                    <div class="col-xs-4">
+                                                                        <img src="{{ asset("storage/$image->src") }}" class="img-responsive img-radio">
+                                                                        <button type="button" class="btn btn-primary btn-radio">Select</button>
+                                                                        <input type="checkbox" id="left-item" class="hidden" name="product_image" value="{{ $image->id }}">
+                                                                    </div>
+                                                            @endforeach
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                                <button type="submit" class="btn btn-default">Submit</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </td>
                         <td class="btn-group">
                             <a
