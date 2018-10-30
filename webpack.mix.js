18aa2e33c25c51e08f2abc9950b8c6f04fd5e369
noureddine belg <nor15dine@gmail.com>
2018-10-30 16:50:20 +0000

    migrate to laravel 5.7

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,10 +18,10 @@ mix
             'node_modules/font-awesome/css/font-awesome.css',
             'node_modules/ionicons/dist/css/ionicons.css',
             'node_modules/select2/dist/css/select2.css',
-            'resources/assets/admin-lte/css/AdminLTE.min.css',
-            'resources/assets/admin-lte/css/skins/skin-purple.min.css',
+            'resources/admin-lte/css/AdminLTE.min.css',
+            'resources/admin-lte/css/skins/skin-purple.min.css',
             'node_modules/datatables/media/css/jquery.dataTables.css',
-            'resources/assets/css/admin.css'
+            'resources/css/admin.css'
         ],
         'public/css/admin.min.css'
     )
@@ -30,18 +30,18 @@ mix
             'node_modules/bootstrap/dist/css/bootstrap.css',
             'node_modules/font-awesome/css/font-awesome.css',
             'node_modules/select2/dist/css/select2.css',
-            'resources/assets/css/drift-basic.min.css',
-            'resources/assets/css/front.css'
+            'resources/css/drift-basic.min.css',
+            'resources/css/front.css'
         ],
         'public/css/style.min.css'
     )
     .scripts(
         [
-            'resources/assets/js/jquery-2.2.3.min.js',
+            'resources/js/jquery-2.2.3.min.js',
             'node_modules/bootstrap/dist/js/bootstrap.js',
             'node_modules/select2/dist/js/select2.js',
             'node_modules/datatables/media/js/jquery.dataTables.js',
-            'resources/assets/admin-lte/js/app.js'
+            'resources/admin-lte/js/app.js'
         ],
         'public/js/admin.min.js'
     )
@@ -49,14 +49,14 @@ mix
         [
             'node_modules/bootstrap/dist/js/bootstrap.js',
             'node_modules/select2/dist/js/select2.js',
-            'resources/assets/js/owl.carousel.min.js',
-            'resources/assets/js/Drift.min.js'
+            'resources/js/owl.carousel.min.js',
+            'resources/js/Drift.min.js'
         ],
         'public/js/front.min.js'
     )
     .copyDirectory('node_modules/datatables/media/images', 'public/images')
     .copyDirectory('node_modules/font-awesome/fonts', 'public/fonts')
-    .copyDirectory('resources/assets/admin-lte/img', 'public/img')
-    .copyDirectory('resources/assets/images', 'public/images')
-    .copy('resources/assets/js/scripts.js', 'public/js/scripts.js')
-    .copy('resources/assets/js/custom.js', 'public/js/custom.js');
+    .copyDirectory('resources/admin-lte/img', 'public/img')
+    .copyDirectory('resources/images', 'public/images')
+    .copy('resources/js/scripts.js', 'public/js/scripts.js')
+    .copy('resources/js/custom.js', 'public/js/custom.js');
