83a6208601d36f122236bb8d0901507826dace4a
Jeff Simons Decena <jeff.decena@yahoo.com>
2018-11-06 19:57:45 +0800

    Add the tw bootstrao cdn in email template

--- resources/views/emails/customer/sendOrderDetailsToCustomer.blade.php
+++ resources/views/emails/customer/sendOrderDetailsToCustomer.blade.php
@@ -7,7 +7,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Order Invoice</title>
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css')}}">
     <style type="text/css">
         table { border-collapse: collapse;}
     </style>
