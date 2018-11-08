8087db8a901c4f2c1f6df2035b2a2ee66e25aee8
noureddine belg <nor15dine@gmail.com>
2018-11-08 04:40:16 +0100

    add make:structure command

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -46,6 +46,11 @@ use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */    
     public function register()
     {
         $this->app->bind(
---
+++ app/Shop/Coupons/Coupon.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Shop\Coupons;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Coupon extends Model
+{
+    //
+
+	protected $fillable = [
+		
+	];
+}
