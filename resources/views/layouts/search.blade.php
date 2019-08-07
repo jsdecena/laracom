642d5de5d6b4dc935856fc6380101da5833a64ad
Noureddine belguinan <nor15dine@gmail.com>
2019-08-07 20:58:25 +0100

    Fix XSS on search form
    This PR is a fix for #210

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -2,7 +2,7 @@
     <!-- search form -->
     <form action="{{$route}}" method="get" id="admin-search">
         <div class="input-group">
-            <input type="text" name="q" class="form-control" placeholder="Search..." value="{!! request()->input('q') !!}">
+            <input type="text" name="q" class="form-control" placeholder="Search..." value="{{ request()->input('q') }}">
             <span class="input-group-btn">
                 <button type="submit" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i> Search </button>
             </span>
