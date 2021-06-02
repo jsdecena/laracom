f9551a3d53618607d45eafed78bd2b0deeaec661
Viktor Maár <viktor.maar@exaktasoftware.se>
2021-06-02 16:30:57 +0200

    Fix symlinks namespacing
    Fix namespacing for symlinked employees requests

--- project/app/Http/Controllers/Auth/LoginController.php
+++ project/app/Http/Controllers/Auth/LoginController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Shop\Admins\Requests\LoginRequest;
+use App\Shop\Employees\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
