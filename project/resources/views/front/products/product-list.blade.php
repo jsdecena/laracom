ee2552dfd28ce09279fb4b7c22445730c04aebcf
ysknsid25 <kengo071225@gmail.com>
2023-02-01 08:41:38 +0900

    #Format
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- project/resources/views/front/products/product-list.blade.php
+++ project/resources/views/front/products/product-list.blade.php
@@ -1,6 +1,6 @@
-@if(!empty($products) && !collect($products)->isEmpty())
+@if (!empty($products) && !collect($products)->isEmpty())
     <ul class="row text-center list-unstyled">
-        @foreach($products as $product)
+        @foreach ($products as $product)
             <li class="col-md-3 col-sm-6 col-xs-12 product-list">
                 <div class="single-product">
                     <div class="product">
@@ -13,17 +13,24 @@
                                                 {{ csrf_field() }}
                                                 <input type="hidden" name="quantity" value="1" />
                                                 <input type="hidden" name="product" value="{{ $product->id }}">
-                                                <button id="add-to-cart-btn" type="submit" class="btn btn-warning" data-toggle="modal" data-target="#cart-modal"> <i class="fa fa-cart-plus"></i> Add to cart</button>
+                                                <button id="add-to-cart-btn" type="submit" class="btn btn-warning"
+                                                    data-toggle="modal" data-target="#cart-modal"> <i
+                                                        class="fa fa-cart-plus"></i> Add to cart</button>
                                             </form>
                                         </li>
-                                        <li>  <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal_{{ $product->id }}"> <i class="fa fa-eye"></i> Quick View</button>
-                                        <li>  <a class="btn btn-default product-btn" href="{{ route('front.get.product', $product->slug) }}"> <i class="fa fa-link"></i> Go to product</a> </li>
+                                        <li> <button type="button" class="btn btn-warning" data-toggle="modal"
+                                                data-target="#myModal_{{ $product->id }}"> <i class="fa fa-eye"></i>
+                                                Quick View</button>
+                                        <li> <a class="btn btn-default product-btn"
+                                                href="{{ route('front.get.product', $product->slug) }}"> <i
+                                                    class="fa fa-link"></i> Go to product</a> </li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
-                        @if(isset($product->cover))
-                            <img src="{{ $product->cover }}" alt="{{ $product->name }}" class="img-bordered img-responsive">
+                        @if (isset($product->cover))
+                            <img src="{{ $product->cover }}" alt="{{ $product->name }}"
+                                class="img-bordered img-responsive">
                         @else
                             <p class="alert alert-warning text-center">Upload cover image.</p>
                         @endif
@@ -33,8 +40,8 @@
                         <h4>{{ $product->name }}</h4>
                         <p>
                             {{ config('cart.currency') }}
-                            @if(!is_null($product->attributes->where('default', 1)->first()))
-                                @if(!is_null($product->attributes->where('default', 1)->first()->sale_price))
+                            @if (!is_null($product->attributes->where('default', 1)->first()))
+                                @if (!is_null($product->attributes->where('default', 1)->first()->sale_price))
                                     {{ number_format($product->attributes->where('default', 1)->first()->sale_price, 2) }}
                                     <p class="text text-danger">Sale!</p>
                                 @else
@@ -46,7 +53,8 @@
                         </p>
                     </div>
                     <!-- Modal -->
-                    <div class="modal fade" id="myModal_{{ $product->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                    <div class="modal fade" id="myModal_{{ $product->id }}" tabindex="-1" role="dialog"
+                        aria-labelledby="myModalLabel">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 @include('layouts.front.product')
@@ -56,7 +64,7 @@
                 </div>
             </li>
         @endforeach
-        @if($products instanceof \Illuminate\Contracts\Pagination\LengthAwarePaginator)
+        @if ($products instanceof \Illuminate\Contracts\Pagination\LengthAwarePaginator)
             <div class="row">
                 <div class="col-md-12">
                     <div class="pull-left">{{ $products->links() }}</div>
